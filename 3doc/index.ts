c94257edbbc9010bd04b5e03ae224dfa9dde8c3c
Giancarlo Bellido <giancarlo@coaxialhost.com>
2026-09-16 11:44:11 -0400

    fix: expose 3doc library API

--- 3doc/index.ts
+++ 3doc/index.ts
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
-import { promises as fs } from 'fs';
-import { join } from 'path';
-import { program, parseArgv } from '@cxl/program';
-import type { Node } from '../dts/index.js';
+import { promises as fs } from "fs";
+import { join } from "path";
+import { program, parseArgv } from "@cxl/program";
+import type { Node } from "../dts/index.js";
 
-import { Parameters, buildDocs } from './render.js';
+import { Parameters, buildDocs } from "./render.js";
+
+export { type BuildDocsOptions, buildDts, buildDocs } from "./render.js";
+export {
+	type SummaryJson,
+	type Summary,
+	renderJson,
+	findExamples,
+} from "./render-summary.js";
+export { Flags, Kind } from "../dts/index.js";
 
 export interface File {
 	name: string;
@@ -13,18 +22,20 @@ export interface File {
 	node?: Node;
 }
 
-await program({}, async ({ log }) => {
-	async function writeFile(file: File, out: string) {
-		const name = file.name;
-		const dest = join(out, name);
-		log(`Writing ${dest}${file.node ? ` from ${file.node.name}` : ''}`);
-		await Promise.all([fs.writeFile(dest, file.content)]);
-	}
+if (import.meta.main) {
+	await program({}, async ({ log }) => {
+		async function writeFile(file: File, out: string) {
+			const name = file.name;
+			const dest = join(out, name);
+			log(`Writing ${dest}${file.node ? ` from ${file.node.name}` : ""}`);
+			await Promise.all([fs.writeFile(dest, file.content)]);
+		}
 
-	try {
-		await buildDocs(parseArgv(Parameters), writeFile);
-	} catch (e) {
-		console.error(e);
-		process.exitCode = 1;
-	}
-})();
+		try {
+			await buildDocs(parseArgv(Parameters), writeFile);
+		} catch (e) {
+			console.error(e);
+			process.exitCode = 1;
+		}
+	})();
+}
